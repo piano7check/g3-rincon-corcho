d19b1612ac9c15a6fa7c18a80bab36f1745c1f83
jhosep-mendoza <jhosdrei25@gmail.com>
2025-04-24 15:26:27 -0400

    areglo
    areglo de la creacion de admin me falto una s a usuarios

--- registrar_usuarios.py
+++ registrar_usuarios.py
@@ -1,17 +1,8 @@
 from conexion_database import get_connection
 import bcrypt
-from validaciones import es_email_valido, es_password_seguro
+
 def insertar_usuario(nombre, correo, password):
-    if not nombre or not correo or not password:
-        return False
-    if not es_email_valido(correo):
-        print("Correo inválido")
-        return False
-    if not es_password_seguro(password):
-        print("Contraseña no segura")
-        return False
     try:
-        
         hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
         conn = get_connection()
